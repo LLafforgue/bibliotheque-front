4bdf8684b1f1ef7baf9345f780b5fba54c112fc6
Ludovic.lafforgue.c@gmail.com <ludovic.lafforgue.c@gmail.com>
2025-09-11 02:03:42 +0200

    #11 ajustements du header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,65 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 import ToggleDarkMode from "../hooks/ToggleDarkMode";
 import {motion} from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faUser, faBook, faStar, faHouse, faBell, faCog, faPlusCircle, faPlus} from '@fortawesome/free-solid-svg-icons';
+import {faUser, faBook, faStar, faRightFromBracket, faBell, faCog, faPlusCircle} from '@fortawesome/free-solid-svg-icons';
 import useMobile from "../hooks/UseMobile";
 
 
 export default function Header() {
     const isMobile = useMobile();
+    const [hasAlter, setHasAlert] = useState(true);
 
   return (
     <div className="w-full p-5 bg-emerald-300 dark:bg-violet-500 text-gray-800 dark:text-gray-50">
      <motion.div
-      initial={{ x: -50, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }} 
+      initial={{ y: -50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }} 
       transition={{ duration: 0.5 }}
       className="flex justify-round items-center"
      >
       <h1 className="text-2xl font-bold">Bibliothèque de User</h1>
 
-      <nav className="mx-[10%] flex flex-wrap gap-4">
-        <NavLink to="/espaces/user" className="hover:underline">
+      <nav className="mx-[10%] flex flex-wrap justify-center gap-6">
+        <NavLink to="/espaces/user" className="flex gap-2 items-center hover:scale-110 transition-transform cursor-pointer">
         <FontAwesomeIcon icon={faUser} className="h-4 w-4 dark:text-white"/>
+        {isMobile||'Paramètres'}
         </NavLink>
         
-        <NavLink to="/espaces" className="hover:underline">
+        <NavLink to="/espaces" className="flex gap-2 items-center hover:scale-110 transition-transform cursor-pointer">
         <FontAwesomeIcon icon={faBook} className="h-4 w-4 dark:text-white"/>
+        {isMobile||'Salles'}
         </NavLink>
 
-        <NavLink to='/espaces/favoris' className="hover:underline">
+        <NavLink to='/espaces/favoris' className="flex gap-2 items-center hover:scale-110 transition-transform cursor-pointer">
         <FontAwesomeIcon icon={faStar} className="h-4 w-4 dark:text-white"/>
+        {isMobile||'Favoris'}
         </NavLink>
 
-        <NavLink to='/espaces/nouvel' className="flex gap-2 items-center hover:underline">
+        <NavLink to='/espaces/nouvel' className="flex gap-2 items-center hover:scale-110 transition-transform cursor-pointer">
         <FontAwesomeIcon icon={faCog} className="h-4 w-4 dark:text-white"/>
-        {isMobile||'Nouvel Salle'}
+        {isMobile||'Gérer les salles'}
         </NavLink>
 
-        <NavLink to='/' className="flex gap-2 items-center hover:underline">
-        <FontAwesomeIcon icon={faHouse} className="h-4 w-4 dark:text-white"/>
+        <NavLink to='/' className="flex gap-2 items-center hover:scale-110 transition-transform cursor-pointer">
+        <FontAwesomeIcon icon={faRightFromBracket} className="h-4 w-4 dark:text-white"/>
         {isMobile||'Déconnexion'}
         </NavLink>
       </nav>
 
-    <div className="flex gap-2">
+    <div className="flex items-center gap-2">
         <motion.div
-        animate={{ rotate: [0, 10, 0] }}
+        animate={hasAlter&&{ rotate: [0, 10, 0] }}
         transition={{
             duration: 1.5,
             ease: "easeInOut",
             repeat: 2,
             repeatType: "reverse",
         }}>
-        <FontAwesomeIcon icon={faBell} className="h-4 w-4 dark:text-white"/>
+        <FontAwesomeIcon icon={faBell} className="h-4 w-4 flex items-center dark:text-white"/>
         </motion.div>
         {isMobile||'Alertes'}
     </div>
-    <div className="flex gap-2 ml-4">
+    <div className="flex items-center gap-2 ml-4">
         <FontAwesomeIcon icon={faPlusCircle} className="h-6 w-6 text-gray-800 dark:text-gray-50 hover:scale-110 transition-transform cursor-pointer"/>
-        {isMobile||'Ajouter une salle'}
+        {isMobile||'Ajouter un lien'}
     </div>
     </motion.div>
     
